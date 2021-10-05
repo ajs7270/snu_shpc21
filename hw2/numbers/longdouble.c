42e3f97b27ae67b9576ef62f597dd53b5eb15daf
jisuAn <ajs7270@naver.com>
2021-10-05 23:35:39 +0900

    Solve longdouble.c

--- hw2/numbers/longdouble.c
+++ hw2/numbers/longdouble.c
@@ -1,9 +1,26 @@
 #include <stdio.h>
+#define NUM_OF_BIT 8
+#define SIZE_OF_LONGDOUBLE 10
 
 int main() {
   long double x;
   scanf("%Lf", &x);
-  // TODO
-  printf("00000000000000000000000000000000000000000000000000000000000000000000000000000000\n");
-  return 0;
+
+    char * c = (char*)&x + SIZE_OF_LONGDOUBLE -1;
+    for (int i =0; i < SIZE_OF_LONGDOUBLE; i++){
+        unsigned char temp = 1 << (NUM_OF_BIT-1);
+        for(int j = 0; j < NUM_OF_BIT; j++){
+            if (temp & *c){
+                printf("%d",1);
+            } else{
+                printf("%d",0);
+            }
+            temp >>= 1;
+        }
+        c -= 1;
+    }
+
+    printf("\n");
+
+    return 0;
 }
