1b9ad7c020e98adb4a315d4f86f4e2667cc16548
jisuAn <ajs7270@naver.com>
2021-10-05 23:04:39 +0900

    Solve long.c

--- hw2/numbers/long.c
+++ hw2/numbers/long.c
@@ -3,7 +3,19 @@
 int main() {
   long x;
   scanf("%ld", &x);
-  // TODO
-  printf("0000000000000000000000000000000000000000000000000000000000000000\n");
+
+    unsigned long temp = 1;
+    temp <<= sizeof(long)*8 - 1;
+
+    for(int i = 0; i<sizeof(long)*8; i++){
+        if(temp & x){
+            printf("%d",1);
+        }else{
+            printf("%d",0);
+        }
+        temp >>= 1;
+    }
+    printf("\n");
+
   return 0;
 }
