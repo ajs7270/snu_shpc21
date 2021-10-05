e20d186c6c700b626132597ac43686e8a8d8f291
jisuAn <ajs7270@naver.com>
2021-10-05 23:14:07 +0900

    Solve float.c

--- hw2/numbers/float.c
+++ hw2/numbers/float.c
@@ -1,9 +1,26 @@
 #include <stdio.h>
+#define NUM_OF_BIT 8
 
 int main() {
   float x;
   scanf("%f", &x);
-  // TODO
-  printf("00000000000000000000000000000000\n");
+
+  char * c = (char*)&x + sizeof(float) - 1;
+
+  for (int i =0; i < sizeof(float); i++){
+      unsigned char temp = 1 << (NUM_OF_BIT-1);
+      for(int j = 0; j < NUM_OF_BIT; j++){
+          if (temp & *c){
+              printf("%d",1);
+          } else{
+              printf("%d",0);
+          }
+          temp >>= 1;
+      }
+      c -= 1;
+  }
+
+  printf("\n");
+
   return 0;
 }
