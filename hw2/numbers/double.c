6b222d1a526a6ee6fd73d652806fd04247d907d3
jisuAn <ajs7270@naver.com>
2021-10-05 23:16:51 +0900

    Sovlve double.c

--- hw2/numbers/double.c
+++ hw2/numbers/double.c
@@ -1,9 +1,26 @@
 #include <stdio.h>
+#define NUM_OF_BIT 8
 
 int main() {
   double x;
   scanf("%lf", &x);
-  // TODO
-  printf("0000000000000000000000000000000000000000000000000000000000000000\n");
+
+    char * c = (char*)&x + sizeof(double) - 1;
+
+    for (int i =0; i < sizeof(double); i++){
+        unsigned char temp = 1 << (NUM_OF_BIT-1);
+        for(int j = 0; j < NUM_OF_BIT; j++){
+            if (temp & *c){
+                printf("%d",1);
+            } else{
+                printf("%d",0);
+            }
+            temp >>= 1;
+        }
+        c -= 1;
+    }
+
+    printf("\n");
+
   return 0;
 }
