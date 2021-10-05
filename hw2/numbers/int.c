a9be5f7203db951696af572a5c6e28ca91aa7570
jisuAn <ajs7270@naver.com>
2021-10-05 22:51:28 +0900

    Solve int.c

--- hw2/numbers/int.c
+++ hw2/numbers/int.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 
 int main() {
-  int x;
-  scanf("%d", &x);
-  // TODO
-  printf("00000000000000000000000000000000\n");
-  return 0;
+    int x;
+    scanf("%d", &x);
+
+    unsigned int temp = 1 << 31;
+
+    for(int i = 0; i<sizeof(int)*8; i++){
+        if(temp & x){
+            printf("%d",1);
+        }else{
+            printf("%d",0);
+        }
+        temp >>= 1;
+    }
+    printf("\n");
+    return 0;
 }
