1aee56c3b334c83265ecc4e29feece0c2bb24c02
jisuAn <ajs7270@naver.com>
2021-11-09 16:56:54 +0900

    Modify multiplication code

--- hw3/mat_mul.c
+++ hw3/mat_mul.c
@@ -4,26 +4,23 @@
 #include <omp.h>
 
 void mat_mul(float *A, float *B, float *C, int M, int N, int K, int num_threads) {
-  // set num of threads
-  omp_set_num_threads(num_threads);
-  
-  // initialize result matrix 
-  #pragma omp parallel
-  zero_mat(C, M, N);
+	// set num of threads
+	omp_set_num_threads(num_threads);
 
+	// initialize result matrix 
   #pragma omp parallel
-  {
-		int r = 16;
-		// parallelize matrix multiplication
-		for (int i = 0; i < M; i++) {
-#pragma omp parallel for schedule (auto)
-			for (int j = 0; j < N; j += r) {
-				for (int k = 0; k < K; k+= r) {
-					//block matrix multiplication
-					for (int jb = j; jb < j+r; jb++){
-						for (int kb = k; kb < k+r; kb++){
-							C[i * N + jb] += A[i*K + kb]*B[kb*N +jb];
-						}
+	zero_mat(C, M, N);
+
+	int r = 16;
+	// parallelize matrix multiplication
+	#pragma omp parallel for schedule (auto)
+	for (int i = 0; i < M; i++) {
+		for (int j = 0; j < N; j += r) {
+			for (int k = 0; k < K; k+= r) {
+				//block matrix multiplication
+				for (int jb = j; jb < j+r; jb++){
+					for (int kb = k; kb < k+r; kb++){
+						C[i * N + jb] += A[i*K + kb]*B[kb*N +jb];
 					}
 				}
 			}
