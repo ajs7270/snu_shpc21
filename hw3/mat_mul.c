cad3d35d6592c18b28ad3c4dc2aaf3041a6b6bea
jisuAn <ajs7270@naver.com>
2021-11-09 14:45:13 +0900

    Modify to cache friendly matrix multiplication

--- hw3/mat_mul.c
+++ hw3/mat_mul.c
@@ -1,13 +1,32 @@
 #include "mat_mul.h"
+#include "util.h"
+
+#include <omp.h>
 
 void mat_mul(float *A, float *B, float *C, int M, int N, int K, int num_threads) {
-  for (int i = 0; i < M; ++i) {
-    for (int j = 0; j < N; ++j) {
-      float s = 0;
-      for (int k = 0; k < K; ++k) {
-        s += A[i * K + k] * B[k * N + j];
-      }
-      C[i * N + j] = s;
-    }
-  }
+  // set num of threads
+  omp_set_num_threads(num_threads);
+  
+  // initialize result matrix 
+  #pragma omp parallel
+  zero_mat(C, M, N);
+
+  #pragma omp parallel
+  {
+		int r = 16;
+		// parallelize matrix multiplication
+		for (int i = 0; i < M; i++) {
+#pragma omp parallel for schedule (auto)
+			for (int j = 0; j < N; j += r) {
+				for (int k = 0; k < K; k+= r) {
+					//block matrix multiplication
+					for (int jb = j; jb < j+r; jb++){
+						for (int kb = k; kb < k+r; kb++){
+							C[i * N + jb] += A[i*K + kb]*B[kb*N +jb];
+						}
+					}
+				}
+			}
+		}
+	}
 }
