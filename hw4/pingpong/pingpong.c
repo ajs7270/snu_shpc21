8a7004ce8083c71e3ce47acb789d5f4da724edb6
jisuAn <ajs7270@naver.com>
2021-11-16 00:50:39 +0900

    Modify pingpong to use MPI_Send and Receive

--- hw4/pingpong/pingpong.c
+++ hw4/pingpong/pingpong.c
@@ -5,9 +5,18 @@
 
 #include "timer.h"
 
+//#define NUM_OF_INT 1024*1024*128
+#define NUM_OF_DOUBLE 10
+
 static int mpi_rank, mpi_size;
 
 int main(int argc, char **argv) {
+	//int *data = (int*)malloc(NUM_OF_INT*sizeof(int)); // GB MB KB
+	double data[NUM_OF_DOUBLE];
+	int tag = 1001;
+
+	MPI_Status status;
+
   MPI_Init(&argc, &argv);
 
   MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
@@ -17,14 +26,15 @@ int main(int argc, char **argv) {
 
   timer_init(1);
   timer_reset(0);
+
   MPI_Barrier(MPI_COMM_WORLD);
   timer_start(0);
-
   if (mpi_rank == 0) {
-    // MPI_Send...
+    MPI_Send(data, NUM_OF_DOUBLE, MPI_DOUBLE, 1, tag, MPI_COMM_WORLD);
     // MPI_Recv...
   }
   else if (mpi_rank == 1) {
+    MPI_Recv(data, NUM_OF_DOUBLE, MPI_DOUBLE, 0, tag, MPI_COMM_WORLD, &status);
     // MPI_Send...
     // MPI_Recv...
   }
